6fb645bfabc620cb4119eff9f5037a5abcfa6ca7
jersearls <searls@me.com>
2018-04-11 00:04:28 -0400

    prevent overflow

--- src/coffeebot.ino
+++ src/coffeebot.ino
@@ -7,18 +7,18 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 // declare global variables
 volatile bool fill = false;
+volatile bool tankFull = false;
 int statusPercent;
 int cups;
 int timeDelay;
 int waterSensor;
-
 //initialize
 void setup() {
   //Serial.begin(9600);
   pinMode(SOLENOID, OUTPUT) ;           //Sets the pin as an output
   Time.zone(-4);
   display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS);
-  ClearScreen();
+  clearScreen();
   //cloud functions
   Particle.function("Stop", Stop);
   Particle.function("TwoCups", FillTwoCups);
@@ -33,41 +33,35 @@ void setup() {
 void loop() {
   waterSensor = analogRead(WATER_SENSOR);
   //Serial.println(waterSensor);
-  if (waterSensor < 1000) {
-    if (fill && statusPercent < 101) {
+  if (waterSensor > 1000 && !tankFull && fill) {
+    tankFull = true;
+    cups = 12;
+  }
+  else if (waterSensor < 1000 || tankFull) {
+    if (!tankFull && fill && statusPercent < 101 ) {
       //digitalWrite(SOLENOID, HIGH) ;    //Switch Solenoid ON
       showMsg(0, 2, "Pouring");
       showMsg(16, 2, String(cups) + " Cups...");
-      StatusBar(statusPercent);
+      statusBar(statusPercent);
       statusPercent += 5;
       delay(timeDelay);
     }
-    else if (fill && statusPercent >= 101) {
+    else if (tankFull || statusPercent >= 101) {
       //digitalWrite(SOLENOID, LOW) ;     //Switch Solenoid OFF
-      ClearScreen();
+      clearScreen();
       String fillDate = Time.format(Time.now(), "%m-%d-%y");
       String fillTime = Time.format(Time.now(), "%I:%M %p");
       showMsg(0, 2, "Filled");
       showMsg(20, 2, String(cups) + " Cups");
       showMsg(40, 1, "Last Filled on:");
       showMsg(50, 1, fillDate + " at " + fillTime);
-      ResetVariables();
+      resetVariables();
     }
-  }
-  else {
-    //digitalWrite(SOLENOID, LOW) ;     //Switch Solenoid OFF
-    ResetVariables();
-    ClearScreen();
-    showMsg(0, 3, "ERROR:");
-    showMsg(32, 2, "Water Tank");
-    showMsg(48, 2, "Full");
-    delay(5000);
-    ClearScreen();
   }
   delay(100);
 }
 
-//private functions
+//local functions camelcase
 void showMsg(int position, int font, String message) {
   display.setTextSize(font);   // 1 = 8 pixel tall, 2 = 16 pixel tall...
   display.setTextColor(WHITE);
@@ -75,64 +69,65 @@ void showMsg(int position, int font, String message) {
   display.println(message);
   display.display();
 }
-void StatusBar(int n) {
+void statusBar(int n) {
   int percent = (n / 100.0) * 128;
   display.drawRect(0, 32, 128, 32, WHITE);
   display.fillRect(0, 32, percent, 32, WHITE);
   display.display();
 }
-void ClearScreen() {
+void clearScreen() {
   display.clearDisplay();
   display.display();
 }
-void ResetVariables() {
+void resetVariables() {
   cups = 0;
   timeDelay = 0;
   statusPercent = 0;
   fill = false;
+  tankFull = false;
 }
-//cloud functions
+//cloud functions pascalcase
 int Stop(String message) {
-  ResetVariables();
-  ClearScreen();
+  resetVariables();
+  clearScreen();
   showMsg(0, 3, "ERROR:");
   showMsg(30, 2, "Emergency");
   showMsg(48, 2, "Stop!");
   delay(5000);
-  ClearScreen();
+  clearScreen();
 }
 int FillTwoCups(String message) {
-  ClearScreen();
+  clearScreen();
   fill = true;
   cups = 2;
   timeDelay = 0;
 }
 int FillFourCups(String message) {
-  ClearScreen();
+  clearScreen();
   fill = true;
   cups = 4;
   timeDelay = 0;
 }
 int FillSixCups(String message) {
-  ClearScreen();
+  clearScreen();
   fill = true;
   cups = 6;
   timeDelay = 0;
 }
 int FillEightCups(String message) {
-  ClearScreen();
+  clearScreen();
   fill = true;
   cups = 8;
   timeDelay = 0;
 }
 int FillTenCups(String message) {
-  ClearScreen();
+  clearScreen();
   fill = true;
   cups = 10;
   timeDelay = 0;
 }
 int FillTwelveCups(String message) {
-  ClearScreen();
+  clearScreen();
   fill = true;
   cups = 12;
   timeDelay = 500;
