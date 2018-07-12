7344ff7649085c044e4a55e517357801a1a028f8
jersearls <searls@me.com>
2018-07-12 14:15:25 -0500

    squash compiler errors

--- src/coffeebot.ino
+++ src/coffeebot.ino
@@ -19,7 +19,7 @@ void setup() {
   pinMode(SOLENOID, OUTPUT) ;         //Sets the solenoid pin as an output
   Time.zone(-4);
   display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS);
-  clearScreen();
+  clearScreen(); //necessary?
   //cloud functions
   Particle.function("Stop", Stop);
   Particle.function("FillWater", FillWater);
@@ -31,23 +31,21 @@ void loop() {
   waterSensor = analogRead(WATER_SENSOR);
   //Serial.println(waterSensor);
   if (waterSensor > 400 && !tankFull && fill) {
-    digitalWrite(SOLENOID, LOW) ;     //Switch Solenoid OFF
+    digitalWrite(SOLENOID, LOW) ; //Switch Solenoid OFF
     tankFull = true;
-    requestedCups = "12";    //max water reservoir capacity
+    requestedCups = "12"; //max water reservoir capacity
     clearScreen();
     stopMessage("Water Tank", "is Full");
   }
   else {
     if (!tankFull && fill && filledOunces < requestedOunces ) {
-      digitalWrite(SOLENOID, HIGH) ;    //Switch Solenoid ON
-      pouringMessage(requestedCups)
-      statusBar(statusPercent);
-      statusPercent += 5 ;
-      }
+        digitalWrite(SOLENOID, HIGH) ;    //Switch Solenoid ON
+        fillingMessage(requestedCups) ;
+        statusBar(filledOunces, requestedOunces);
     }
     else if (tankFull || filledOunces >= requestedOunces) {
       digitalWrite(SOLENOID, LOW) ;     //Switch Solenoid OFF
-      fillMessage(requestedCups) ;
+      fillingMessage(requestedCups) ;
       resetVariables() ;
     }
   }
@@ -56,54 +54,50 @@ void loop() {
 //local functions (camelcase)
 //LCD functions
 void showMsg(int position, int font, String message) {
-  display.setTextSize(font);   // 1 = 8 pixel tall, 2 = 16 pixel tall...
-  display.setTextColor(WHITE);
-  display.setCursor(0, position);
-  display.println(message);
-  display.display();
+  display.setTextSize(font) ;   // 1 = 8 pixel tall, 2 = 16 pixel tall...
+  display.setTextColor(WHITE) ;
+  display.setCursor(0, position) ;
+  display.println(message) ;
+  display.display() ;
 }
-void statusBar(int n) {
-  int percent = (n / 100.0) * 128;
-  display.drawRect(0, 32, 128, 32, WHITE);
-  display.fillRect(0, 32, percent, 32, WHITE);
-  display.display();
+void statusBar(int filledOunces, int requestedOunces) {
+  int percent = (filledOunces / requestedOunces) * 128 ;
+  display.drawRect(0, 32, 128, 32, WHITE) ;
+  display.fillRect(0, 32, percent, 32, WHITE) ;
+  display.display() ;
 }
 void clearScreen() {
   display.clearDisplay() ;
   display.display() ;
 }
-void stopMessage() {
-  clearScreen();
-  showMsg(0, 3, "ERROR:");
-  showMsg(30, 2, line1);
-  showMsg(48, 2, line2);
-  delay(3000);
+void stopMessage(String line1, String line2) {
+  clearScreen() ;
+  showMsg(0, 3, "ERROR:") ;
+  showMsg(30, 2, line1) ;
+  showMsg(48, 2, line2) ;
+  delay(3000) ;
 }
 void filledMessage(String cups) {
-  clearScreen();
-  String fillDate = Time.format(Time.now(), "%m-%d-%y");
-  String fillTime = Time.format(Time.now(), "%I:%M %p");
-  showMsg(0, 2, "Filled");
-  showMsg(20, 2, cups + " Cups");
-  showMsg(40, 1, "Last Filled on:");
-  showMsg(50, 1, fillDate + " at " + fillTime);
+  clearScreen() ;
+  String fillDate = Time.format(Time.now(), "%m-%d-%y") ;
+  String fillTime = Time.format(Time.now(), "%I:%M %p") ;
+  showMsg(0, 2, "Filled") ;
+  showMsg(20, 2, cups + " Cups") ;
+  showMsg(40, 1, "Last Filled on:") ;
+  showMsg(50, 1, fillDate + " at " + fillTime) ;
 }
-void pouringMessage(String cups) {
+void fillingMessage(String cups) {
   showMsg(0, 2, "Pouring");
   showMsg(16, 2, cups + " Cups...");
 }
 //flowsensor functions
-void cupsToOunces(String cups) {
-  intCup = cups.toInt();
-  ounces = intCup * 5;
-  return ounces;
-}
+
 //control flow functions
 void resetVariables() {
-  cups = 0;
-  statusPercent = 0;
-  fill = false;
-  tankFull = false;
+  requestedOunces = 0 ;
+  filledOunces = 0 ;
+  fill = false ;
+  tankFull = false ;
 }
 
 //cloud functions (pascalcase)
@@ -117,7 +111,7 @@ int FillWater(String message) {
   if (!fill) {
     clearScreen() ;
     requestedCups = message ;
-    requestedOunces = cupsToOunces(message)
+    requestedOunces = message.toInt() * 5;
     fill = true ;
   }
 }
